fea3cd65513a6c45571b03f3ad3b53a547bb5f7d
Alex <malgelir@icloud.com>
2018-06-11 11:07:17 +0100

    Initial Commit

---
+++ SlidingSegmentedControl/SlidingSegmentedControl.h
@@ -0,0 +1,19 @@
+//
+//  SlidingSegmentedControl.h
+//  SlidingSegmentedControl
+//
+//  Created by Alex on 11/06/2018.
+//  Copyright © 2018 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SlidingSegmentedControl.
+FOUNDATION_EXPORT double SlidingSegmentedControlVersionNumber;
+
+//! Project version string for SlidingSegmentedControl.
+FOUNDATION_EXPORT const unsigned char SlidingSegmentedControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SlidingSegmentedControl/PublicHeader.h>
+
+
